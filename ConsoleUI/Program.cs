05f8d71e25402f9cc6f5624961b90e97fb7b7660
Enes Yalçın <enesylcn14@gmail.com>
2026-03-05 13:54:27 +0300

    Katmanlar oluşturuldu

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,2 +1,85 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+/*int[] array = { 2, 3, 4, 5, 1, 7};
+
+for (int i = 0; i < array.Length; i++)
+{
+    if()
+}*/
+
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            double temp=0;
+            for (int i = 1; i < 11; i++)
+            {
+                temp = temp + Math.Pow(i, 3);
+            }
+            Console.WriteLine(temp);
+            */
+            /*
+            int yil = 2026;
+            DateTime dateTime = DateTime.Now;   
+            Console.WriteLine("Doğum yılınızı giriniz");
+            DateTime dogumTarihi = Convert.ToDateTime(Console.ReadLine());
+
+            TimeSpan sonuc = dateTime - dogumTarihi;
+            Console.WriteLine("Yaşınız: " +(sonuc.Days)/365);
+            */
+            /*
+            Console.WriteLine("Lütfen Bir sayı giriniz.");
+            int toplam = 1;
+            int sayi=int.Parse(Console.ReadLine());
+            if (sayi == 0)
+            {
+                Console.WriteLine(toplam);
+            }
+            else
+            {
+                for (int i = sayi; i > 0; i--)
+                {
+                    toplam = toplam * i;
+
+
+                }
+                Console.WriteLine(toplam);  
+            }
+            *//*
+            Console.WriteLine("Lütfen sayıları giriniz.");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int toplam = 0;
+            
+            for (int i = 0; i < a; i++)
+            {
+                  toplam += b;
+                
+            }
+            Console.WriteLine(toplam);
+            */
+
+        public class Solution
+        {
+            public bool IsPalindrome(int x)
+            {
+                if(x < 0) return false;
+
+                while (x > 0) 
+                { 
+                    
+                }
+
+
+                return true;
+            }
+        }
+
+
+    
+    }
+}
